5c8cade997773351954f4cdf9d72ac22e0e7a4e8
pheelgit <philatov63@mail.ru>
2023-05-30 06:18:46 +0300

    add vercel analytics

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import { Layout } from 'components';
 import { Header, SideBar } from 'widgets';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 import './App.module.scss';
 
 function App() {
   return (
-    <Layout header={<Header />} sidebar={<SideBar />} content={<Outlet />} />
+    <>
+      <Analytics />
+      <Layout header={<Header />} sidebar={<SideBar />} content={<Outlet />} />
+    </>
   );
 }
 
